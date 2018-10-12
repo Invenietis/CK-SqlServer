3fef63ec449287e2b9b8f1ca059c06c16ec02136
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-10-12 18:34:25 +0200

    Using new IsModelDependent attribute.

--- CK.SqlServer/StObjSupport/AssemblyInfo.cs
+++ CK.SqlServer/StObjSupport/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System;
 // This enables the SqlStandardCallContext that is a IScopedAmbientService to be handled by
 // CK.StObj automatic DI.
 //
-[assembly: CK.Setup.IsModel()]
-namespace CK.Setup { class IsModelAttribute : Attribute {} }
+[assembly: CK.Setup.IsModelDependent()]
+namespace CK.Setup { class IsModelDependentAttribute : Attribute {} }
 
